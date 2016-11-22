2045fc2143b8a27fe0f9cd9b01d15ae115f0ce3e
melissa <melissa160@gmail.com>
2016-11-22 17:12:34 -0500

    Quotes machine ready with jquery

---
+++ app.js
@@ -0,0 +1,23 @@
+$('.btn').on('click', function() {
+	$.getJSON("http://quotes.stormconsultancy.co.uk/quotes.json",function(json) {
+    //$(".quote").html(JSON.stringify(json));
+    var html = "";  
+    var author = "";
+    randomQuote = Math.floor(Math.random()*(45-1)+1);
+    json = json.filter(function(val) {
+      console.log(val.id);
+      console.log("ram" + randomQuote);
+      return (val.id === randomQuote);
+    });
+
+    console.log(json);
+    html += "<p>"+ json[0].quote
+    html += "</p>"
+
+    author += "<p>"+ json[0].author
+    author += "</p>"
+
+    $(".txt").html(html);
+    $(".author").html(author);
+  });
+});
